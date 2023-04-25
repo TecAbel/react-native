b5b050f3203b389da2eb153d7a2ed3aaef44c2d1
TecAbel <abelardo96.work@gmail.com>
2023-04-25 10:22:42 -0600

    feat(paths): absolute paths added

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
+import { HomePage } from '@src/pages/home/home'
 import { Provider } from 'react-redux'
-import { HomePage } from './src/pages/home/home'
 import store from './state/store'
 
 export default function App() {
